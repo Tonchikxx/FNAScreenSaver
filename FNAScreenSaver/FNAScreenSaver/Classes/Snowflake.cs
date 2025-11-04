10286062d0074b08b4153092da14113803bfc9b3
Tonchikxx <ustikantonina@gmail.com>
2025-11-05 01:44:28 +0300

    Итог

---
+++ FNAScreenSaver/FNAScreenSaver/Classes/Snowflake.cs
@@ -0,0 +1,28 @@
+﻿namespace FNAScreenSaver.Classes
+{
+    /// <summary>
+    /// Параметры снежинок
+    /// </summary>
+    public class Snowflake
+    {
+        /// <summary>
+        /// Местоположение снежинки по X
+        /// </summary>
+        public int X { get; set; }
+
+        /// <summary>
+        /// Местоположение снежинки по Y
+        /// </summary>
+        public int Y { get; set; }
+
+        /// <summary>
+        /// Размер снежинки
+        /// </summary>
+        public int Size { get; set; }
+
+        /// <summary>
+        /// Скорость снежинки
+        /// </summary>
+        public int Speed { get; set; }
+    }
+}
